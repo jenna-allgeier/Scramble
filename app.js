fad39028fe3784aa0d0a81ac991148d0b48bb649
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-03-10 22:07:17 -0500

    New 5-Letter Word Bank imported.

--- app.js
+++ app.js
@@ -13,13 +13,45 @@ const letterSet = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "b", "b", "c", "
 "o", "o", "o", "o", "o", "o", "o", "o", "p", "p", "q", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", 
 "t", "t", "t", "t", "t", "t", "u", "u", "u", "u", "v", "v", "w", "w", "x", "y", "y", "z"];
 
-const wordBank = ["fiend", "brand", "swarm", "trout", "brain", "hazy", "queen", "exits", "mouse", "arise", 
-"horse", "point", "alone", "empty", "slate", "place", "light", "mount", "doubt", "query", "found", "chalk",
-"state", "sound", "water", "joint", "mined", "tango", "gloat", "brine", "prize", "loser", "liver", "timer",
-"proud", "wreck", "tinge", "giver", "scorn", "haven", "teeth", "worth", "wrath", "great", "grate", "wrong",
-"timed", "taped", "today", "paved", "prove", "voter", "hoped", "guess", "tacky", "slime", "hatch", "brick", 
-"rower", "miner", "waxed", "maxed", "tired", "valve", "those", "cuter", "round", "stick", "knick", "snack",
-"grace"]
+const wordBank = ["about","above","abuse","actor","acute","admit","adopt","adult","after","again","agent",
+"agree","ahead","alarm","album","alert","alike","alive","allow","alone","along","alter","among","anger",
+"Angle","angry","apart","apple","apply","arena","argue","arise","array","aside","asset","audio","audit",
+"avoid","award","aware","badly","baker","bases","basic","basis","beach","began","begin","begun","being",
+"below","bench","billy","birth","black","blame","blind","block","blood","board","boost","booth","bound",
+"brain","brand","bread","break","breed","brief","bring","broad","broke","brown","build","built","buyer",
+"cable","calif","carry","catch","cause","chain","chair","chart","chase","cheap","check","chest","chief",
+"child","china","chose","civil","claim","class","clean","clear","click","clock","close","coach","coast",
+"could","count","court","cover","craft","crash","cream","crime","cross","crowd","crown","curve","cycle",
+"daily","dance","dated","dealt","death","debut","delay","depth","doing","doubt","dozen","draft","drama",
+"drawn","dream","dress","drill","drink","drive","drove","dying","eager","early","earth","eight","elite",
+"empty","enemy","enjoy","enter","entry","equal","error","event","every","exact","exist","extra","faith",
+"false","fault","fiber","field","fifth","fifty","fight","final","first","fixed","flash","fleet","floor",
+"fluid","focus","force","forth","forty","forum","found","frame","frank","fraud","fresh","front","fruit",
+"fully","funny","giant","given","glass","globe","going","grace","grade","grand","grant","grass","great",
+"green","gross","group","grown","guard","guess","guest","guide","happy","harry","heart","heavy","hence",
+"henry","horse","hotel","house","human","ideal","image","index","inner","input","issue","japan","jimmy",
+"joint","jones","judge","known","label","large","laser","later","laugh","layer","learn","lease","least",
+"leave","legal","level","lewis","light","limit","links","lives","local","logic","loose","lower","lucky",
+"lunch","lying","magic","major","maker","march","maria","match","maybe","mayor","meant","media","metal",
+"might","minor","minus","mixed","model","money","month","moral","motor","mount","mouse","mouth","movie",
+"music","needs","never","newly","night","noise","north","noted","novel","nurse","occur","ocean","offer",
+"often","order","other","ought","paint","panel","paper","party","peace","peter","phase","phone","photo",
+"piece","pilot","pitch","place","plain","plane","plant","plate","point","pound","power","press","price",
+"pride","prime","print","prior","prize","proof","proud","prove","queen","quick","quiet","quite","radio",
+"raise","range","rapid","ratio","reach","ready","refer","right","rival","river","robin","roger","roman",
+"rough","round","route","royal","rural","scale","scene","scope","score","sense","serve","seven","shall",
+"shape","share","sharp","sheet","shelf","shell","shift","shirt","shock","shoot","short","shown","sight",
+"since","sixth","sixty","sized","skill","sleep","slide","small","smart","smile","smith","smoke","solid",
+"solve","sorry","sound","south","space","spare","speak","speed","spend","spent","split","spoke","sport",
+"staff","stage","stake","stand","start","state","steam","steel","stick","still","stock","stone","stood",
+"store","storm","story","strip","stuck","study","stuff","style","sugar","suite","super","sweet","table",
+"taken","taste","taxes","teach","teeth","terry","texas","thank","theft","their","theme","there","these",
+"thick","thing","think","third","those","three","threw","throw","tight","times","tired","title","today",
+"topic","total","touch","tough","tower","track","trade","train","treat","trend","trial","tried","tries",
+"truck","truly","trust","truth","twice","under","undue","union","unity","until","upper","upset","urban",
+"usage","usual","valid","value","video","virus","visit","vital","voice","waste","watch","water","wheel",
+"where","which","while","white","whole","whose","woman","women","world","worry","worse","worst","worth",
+"would","wound","write","wrong","wrote","yield","young","youth"]
 
 let startingLetters = [];
 
@@ -137,6 +169,16 @@ const compareUserToWordBank = (gridLetters) => {
     }
 }
 
+// let firstColumn = ((gridItem[0].innerText) + (gridItem[5].innerText) + (gridItem[10].innerText) + (gridItem[15].innerText) + (gridItem)[20].innerText);
+// let secondColumn = ((gridItem[1].innerText) + (gridItem[6].innerText) + (gridItem[11].innerText) + (gridItem[16].innerText) + (gridItem)[21].innerText);
+// let thirdColumn = ((gridItem[2].innerText) + (gridItem[7].innerText) + (gridItem[12].innerText) + (gridItem[17].innerText) + (gridItem)[22].innerText);
+// let fourthColumn = ((gridItem[3].innerText) + (gridItem[8].innerText) + (gridItem[13].innerText) + (gridItem[18].innerText) + (gridItem)[23].innerText);
+// let fifthColumn = ((gridItem[4].innerText) + (gridItem[9].innerText) + (gridItem[14].innerText) + (gridItem[19].innerText) + (gridItem)[24].innerText);
+
+// console.log(firstColumn)
+// console.log(thirdColumn)
+// console.log(fifthColumn)
+
 
 
 const playAgain = () => {
@@ -205,12 +247,4 @@ replay.addEventListener("click", playAgain);
 
 
 
-  // let firstColumn = ((gridLetters[0]) + (gridLetters[5]) + (gridLetters[10]) + (gridLetters[15]) + (gridLetters)[20]);
-    // let secondColumn = ((gridLetters[1]) + (gridLetters[6]) + (gridLetters[11]) + (gridLetters[16]) + (gridLetters)[21]);
-    // let thirdColumn = ((gridLetters[2]) + (gridLetters[7]) + (gridLetters[12]) + (gridLetters[17]) + (gridLetters)[22]);
-    // let fourthColumn = ((gridLetters[3]) + (gridLetters[8]) + (gridLetters[13]) + (gridLetters[18]) + (gridLetters)[23]);
-    // let fifthColumn = ((gridLetters[4]) + (gridLetters[9]) + (gridLetters[14]) + (gridLetters[19]) + (gridLetters)[24]);
-
-    // console.log(firstColumn)
-    // console.log(thirdColumn)
-    // console.log(fifthColumn)
+ 
